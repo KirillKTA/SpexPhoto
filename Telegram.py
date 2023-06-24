3e50b8b35cf5b811af96424489e91d98964ae46d
KirillKTA <Kirilltopkasov@gmail.com>
2023-06-24 17:45:25 +1000

    исправил сделал правки

--- Telegram.py
+++ Telegram.py
@@ -3,18 +3,32 @@ import os
 import random
 import time
 from dotenv import load_dotenv
+import argparse
 
 
 if __name__ == '__main__':
     load_dotenv()
     bot = telegram.Bot(token=os.environ['TELEGRAM_TOKEN'])
+    telegram_id=os.environ['TELEGRAM_ID']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--time", help="Вставьте с какой переодичностью отпрвлять фотографии (в секундах)",
+                        default='14400')
+    args = parser.parse_args()
+    periodicity = int(args.time)
+
     while True:
-        images = os.listdir("images")
-        random_image = random.choice(images)
-        image_path = os.path.join("images", random_image)
-        with open(image_path, 'rb') as photo:
-            bot.send_photo(chat_id=os.environ['TELEGRAM_ID'], photo=photo)
-        time.sleep(14400)
+
+        all_files = os.walk("images")
+
+        for array_of_files in all_files:
+            folder, nested_folder, files = array_of_files
+            random.shuffle(files)
+            for image in files:
+                path = os.path.join('images', image)
+                with open(path, 'rb') as file:
+                    bot.send_document(chat_id=telegram_id, document=file)
+                time.sleep(5)
+        time.sleep(periodicity)
 
 
 

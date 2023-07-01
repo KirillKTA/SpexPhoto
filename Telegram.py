923668a99728c6cb8898c3883b103900b1c4c4a4
KirillKTA <Kirilltopkasov@gmail.com>
2023-07-01 17:47:06 +1000

    правки

--- Telegram.py
+++ Telegram.py
@@ -11,24 +11,27 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=os.environ['TELEGRAM_TOKEN'])
     telegram_id=os.environ['TELEGRAM_ID']
     parser = argparse.ArgumentParser()
-    parser.add_argument("--time", help="Вставьте с какой переодичностью отпрвлять фотографии (в секундах)",
+    parser.add_argument("--time",type=int, help="Вставьте с какой переодичностью отпрлять фотографии (в секундах)",
                         default='14400')
     args = parser.parse_args()
-    periodicity = int(args.time)
+    print(args.time)
 
     while True:
+        try:
+            all_files = os.walk("images")
 
-        all_files = os.walk("images")
+            for array_of_files in all_files:
+                folder, nested_folder, files = array_of_files
+                random.shuffle(files)
+                for image in files:
+                    path = os.path.join('images', image)
+                    with open(path, 'rb') as file:
+                        bot.send_document(chat_id=telegram_id, document=file)
+                    time.sleep(5)
+            time.sleep(args.time)
+        except telegram.error.NetworkError:
+            print("соединение прерванно")
 
-        for array_of_files in all_files:
-            folder, nested_folder, files = array_of_files
-            random.shuffle(files)
-            for image in files:
-                path = os.path.join('images', image)
-                with open(path, 'rb') as file:
-                    bot.send_document(chat_id=telegram_id, document=file)
-                time.sleep(5)
-        time.sleep(periodicity)
 
 
 

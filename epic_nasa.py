5823952ace995a12b9372fc72d764d6d85373dab
KirillKTA <Kirilltopkasov@gmail.com>
2023-08-26 18:42:29 +1000

    Офорление кода по pep8 epic_nasa.py

--- epic_nasa.py
+++ epic_nasa.py
@@ -8,13 +8,15 @@ from dotenv import load_dotenv
 def fetch_nasa_epic_images(apikey):
 
     payload = {"api_key": apikey, }
-    response = requests.get('https://api.nasa.gov/EPIC/api/natural/images', params=payload)
+    response = requests.get('https://api.nasa.gov/EPIC/api/natural/images',
+                            params=payload)
     response.raise_for_status()
     for number, link in enumerate(response.json()):
         image_path = f'{number} epic.jpg'
         date = datetime.fromisoformat(link["date"])
         date = date.strftime("%Y/%m/%d")
-        image_link = f"https://api.nasa.gov/EPIC/archive/natural/{date}/png/{link['image']}.png"
+        image_link = f"https://api.nasa.gov/EPIC/" \
+                     f"archive/natural/{date}/png/{link['image']}.png"
         download_image(image_link, image_path, params=payload)
 
 
